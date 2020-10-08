6f801118cac5215839313f74abad63fb40ecce27
Jah <moskovienkor@gmail.com>
2020-10-08 15:11:13 +0300

    hibernate3: - travis fix;

--- src/main/java/com/dev/cinema/security/AuthenticationServiceImpl.java
+++ src/main/java/com/dev/cinema/security/AuthenticationServiceImpl.java
@@ -1,7 +1,6 @@
 package com.dev.cinema.security;
 
 import com.dev.cinema.exceptions.AuthenticationException;
-import com.dev.cinema.exceptions.DataProcessingException;
 import com.dev.cinema.lib.Inject;
 import com.dev.cinema.lib.Service;
 import com.dev.cinema.model.User;
