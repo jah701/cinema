13d3756f86a307f70078bd181ee5c2b5b8eeaecb
Jah <moskovienkor@gmail.com>
2020-10-10 17:41:16 +0300

    hibernate4: - travis;

--- src/main/java/com/dev/cinema/service/impl/UserServiceImpl.java
+++ src/main/java/com/dev/cinema/service/impl/UserServiceImpl.java
@@ -4,7 +4,6 @@ import com.dev.cinema.dao.UserDao;
 import com.dev.cinema.lib.Inject;
 import com.dev.cinema.lib.Service;
 import com.dev.cinema.model.User;
-import com.dev.cinema.service.ShoppingCartService;
 import com.dev.cinema.service.UserService;
 import com.dev.cinema.util.HashUtil;
 import java.util.Optional;
