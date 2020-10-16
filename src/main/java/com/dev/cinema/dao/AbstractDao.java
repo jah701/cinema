01b738fa9dc57ea2ce0dd4ba79f07f42666663ea
Jah <moskovienkor@gmail.com>
2020-10-16 15:07:53 +0300

    logger: - done;

---
+++ src/main/java/com/dev/cinema/dao/AbstractDao.java
@@ -0,0 +1,34 @@
+package com.dev.cinema.dao;
+
+import com.dev.cinema.exceptions.DataProcessingException;
+import com.dev.cinema.util.HibernateUtil;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class AbstractDao<T> {
+    private static final Logger logger = Logger.getLogger(AbstractDao.class);
+
+    public T add(T entity, Class<T> clazz) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(entity);
+            transaction.commit();
+            return entity;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add new "
+                    + clazz.getSimpleName() + " entity", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+            logger.info(entity + " was added successfully!");
+        }
+    }
+}
