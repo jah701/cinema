77a5f61776ebb0ec8b57bac7e42008cbe3c5423e
Jah <moskovienkor@gmail.com>
2020-10-24 16:27:27 +0300

    spring-mapper-dto: - review fix;

--- src/main/java/com/dev/cinema/controller/AuthenticationController.java
+++ src/main/java/com/dev/cinema/controller/AuthenticationController.java
@@ -5,11 +5,9 @@ import com.dev.cinema.model.dto.UserRequestDto;
 import com.dev.cinema.security.AuthenticationService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
